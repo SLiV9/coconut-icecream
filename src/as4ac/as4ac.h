a16bf92b634360af1f8f095d22eed30a88b6b53d
SLiV <sanderintveld@hotmail.com>
2014-02-11 17:03:46 +0100

    as4: arithmetic operator counting werkt

---
+++ src/as4ac/as4ac.h
@@ -0,0 +1,9 @@
+#ifndef _AS4AC_H_
+#define _AS4AC_H_
+
+#include "types.h"
+
+extern node* AS4ACbinop(node *arg_node, info *arg_info);
+extern node *AS4ACdoCount( node *syntaxtree);
+
+#endif /* _AS4AC_H_ */
