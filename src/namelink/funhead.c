b1d707ea2b7e044818812041fded7757ac8e4b47
SLiV <sanderintveld@hotmail.com>
2014-03-11 17:27:42 +0100

    redeclarations geven errors

--- src/namelink/funhead.c
+++ src/namelink/funhead.c
@@ -6,26 +6,18 @@
 #include "tree_basic.h"
 #include "traverse.h"
 #include "dbug.h"
+#include "ctinfo.h"
 #include "memory.h"
 #include "str.h"
+#include "myglobals.h"
 
-/*
- * INFO structure
- */
-struct INFO {
-  node *stack;
-  int depth;
-};
-
-#define INFO_STACK(n) ((n)->stack)
-#define INFO_DEPTH(n) ((n)->depth)
+#include "namestackinfo.h"
 
 node* NAMELINKFUNHEADfundef(node *arg_node, info *arg_info)
 {
   DBUG_ENTER ("NAMELINKFUNHEADfundef");
 
-	INFO_STACK( arg_info) = TBmakeNamedecs( HEADER_NAME(FUNDEF_HEAD(arg_node)), \
-			INFO_DEPTH( arg_info), arg_node, INFO_STACK( arg_info));
+	pushNameDec( arg_node, arg_info, HEADER_NAME(FUNDEF_HEAD(arg_node)));
 
   DBUG_RETURN (arg_node);
 }
@@ -34,8 +26,7 @@ node* NAMELINKFUNHEADfundec(node *arg_node, info *arg_info)
 {
   DBUG_ENTER ("NAMELINKFUNHEADfundec");
 
-	INFO_STACK( arg_info) = TBmakeNamedecs( HEADER_NAME(FUNDEC_HEAD(arg_node)), \
-			INFO_DEPTH( arg_info), arg_node, INFO_STACK( arg_info));
+	pushNameDec( arg_node, arg_info, HEADER_NAME(FUNDEC_HEAD(arg_node)));
 
   DBUG_RETURN (arg_node);
 }
